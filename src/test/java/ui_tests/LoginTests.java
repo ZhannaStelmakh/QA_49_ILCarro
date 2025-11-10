23684f9f8afc07d43e4db1b1a7cbe264a2a43ff1
ZhannaStelmakh <vertebrology.center@gmail.com>
2025-11-10 09:20:54 +0200

    added logger

--- src/test/java/ui_tests/LoginTests.java
+++ src/test/java/ui_tests/LoginTests.java
@@ -8,14 +8,17 @@ import pages.HomePage;
 import pages.LoginPage;
 import utils.RetryAnalyzer;
 
+import java.lang.reflect.Method;
+
 public class LoginTests extends ApplicationManager {
 
     @Test(retryAnalyzer = RetryAnalyzer.class)
-    public void loginPositiveTest() {
+    public void loginPositiveTest(Method method) {
         User user = User.builder()
                 .username("cherry@gmail.com")
                 .password("Ch12345$")
                 .build();
+        logger.info("start test" + method.getName() +" with date" + user);
         new HomePage(getDriver()).clickBtnLoginHeader();
         LoginPage loginPage = new LoginPage(getDriver());
         loginPage.typeLoginForm(user);
